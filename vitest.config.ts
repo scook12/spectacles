5f4c823abf71fa797cfad86cca8c84dcd0616805
Sam Cook <rigdoncook@gmail.com>
2026-06-26 19:01:48 -0700

    conf: test cov details

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    coverage: {
+      provider: 'v8',
+      exclude: ['examples/', 'dist/', 'vitest.config.ts'],
+      include: ['src/**/*.ts']
+    },
     include: ['test/**/*.test.ts'],
     exclude: [
       'dist/**',
